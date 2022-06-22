1109edcfcf28659af0811b38902db4023b2e840c
Ignatella <ignatellasecond@gmail.com>
2022-06-22 10:30:04 +0200

    Add: main form title

--- Project48/BaseFrame.h
+++ Project48/BaseFrame.h
@@ -77,7 +77,7 @@ protected:
 
 public:
 
-	BaseFrame(wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(800, 600), long style = wxDEFAULT_FRAME_STYLE | wxTAB_TRAVERSAL);
+	BaseFrame(wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxT("Foto-Patchwork"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(800, 600), long style = wxDEFAULT_FRAME_STYLE | wxTAB_TRAVERSAL);
 
 	~BaseFrame();
 
